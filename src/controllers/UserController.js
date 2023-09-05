4db0fcdc5adedc41d35bf3624c3d681568f65d79
Enoque Rogerio <enoquerogerio15mti25@gmail.com>
2023-09-05 21:00:19 +0100

    feature: delete

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,117 +1,118 @@
-import User from '../models/User'
-import bcrypt from 'bcrypt'
+import User from "../models/User";
+import bcrypt from "bcrypt";
 
-class UserController{
-    async store(req, res){
-        try{
-            const { email, password } = req.body;
+class UserController {
+  async store(req, res) {
+    try {
+      const { email, password } = req.body;
 
-            //Validate input
-            if(!(email && password)){
-                res.status(400).send('All input is required');
-                return;
-            }
+      //Validate input
+      if (!(email && password)) {
+        res.status(400).send("All input is required");
+        return;
+      }
 
-            //Check if user already exist
-            const oldUser = await User.findOne({email});
+      //Check if user already exist
+      const oldUser = await User.findOne({ email });
 
-            if(oldUser){
-                res.status(409).send('User already exist. Please Login');
-                return;
-            }
+      if (oldUser) {
+        res.status(409).send("User already exist. Please Login");
+        return;
+      }
 
-            //encrypt user password
-            const encryptedPassword = await bcrypt.hash(password, 8);
+      //encrypt user password
+      const encryptedPassword = await bcrypt.hash(password, 8);
 
-            //create user in database
-            const user = await User.create({
-                email: email.toLowerCase(),
-                password: encryptedPassword,
-            })
-            return res.status(201).json({message: 'Person successfully created'})
-        }catch(error){
-            console.log(error);
-            return;
-        }
+      //create user in database
+      const user = await User.create({
+        email: email.toLowerCase(),
+        password: encryptedPassword,
+      });
+      return res.status(201).json({ message: "Person successfully created" });
+    } catch (error) {
+      console.log(error);
+      return;
     }
-    async index(req, res){
-        try{
-            const user = await User.find();
-            return res.json(user);
-        }catch(error){
-            console.log(error);
-        }
-
+  }
+  async index(req, res) {
+    try {
+      const user = await User.find();
+      return res.json(user);
+    } catch (error) {
+      console.log(error);
     }
-    async show(req, res){
-        try{
-            const user = await User.findById(req.params.id);
-            return res.json(user);
-        }catch(error){
-            return res.json(null)
-        }
+  }
+  async show(req, res) {
+    try {
+      const user = await User.findById(req.params.id);
+      return res.json(user);
+    } catch (error) {
+      return res.json(null);
     }
+  }
 
-    async update(req, res){
-        try{
-            if(!req.params.id){
-                return res.status(400).json({
-                    message: 'Missing id'
-                })
-            }
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          message: "Missing id",
+        });
+      }
 
-            const user = await User.findById(req.params.id);
+      const user = await User.findById(req.params.id);
 
-            if(!user){
-                return res.status(422).json({
-                    message: 'User not found'
-                })
-            }
+      if (!user) {
+        return res.status(422).json({
+          message: "User not found",
+        });
+      }
 
-            //check if the email already exists
-            if(await User.findOne(req.params.email)){
-                return res.status(400).json({
-                    message: 'Email already exits'
-                })
-            }
-
-            if(req.body.password){
-                req.body.password = await bcrypt.hash(req.body.password, 8);
-            }
-
-
-            const newUser = await User.updateOne({_id: req.params.id}, req.body)
-            return res.json(newUser);
-        }catch(error){
-            console.log(error)
+      //check if the email already exists
+      if (req.body.email) {
+        const { email } = req.body;
+        if (await User.findOne({email})) {
+          return res.status(400).json({
+            message: "Email already exits",
+          });
         }
+      }
+
+      if (req.body.password) {
+        req.body.password = await bcrypt.hash(req.body.password, 8);
+      }
+
+      const newUser = await User.updateOne({ _id: req.params.id }, req.body);
+      return res.json(newUser);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    async delete(req, res){
-        try{
-            if(!req.params.id){
-                return res.status(400).json({
-                    message: 'Missing id'
-                })
-            }
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          message: "Missing id",
+        });
+      }
 
-            const user = await User.findById(req.params.id);
+      const user = await User.findById(req.params.id);
 
-            if(!user){
-                return res.status(422).json({
-                    message: 'User not found'
-                })
-            }
+      if (!user) {
+        return res.status(422).json({
+          message: "User not found",
+        });
+      }
 
-            await User.deleteOne({_id: req.params.id});
-            res.status(200).json({message: 'User successfully deleted'});
-        }catch(error){
-            console.log(error);
-        }
+      await User.deleteOne({ _id: req.params.id });
+      res.status(200).json({ message: "User successfully deleted" });
+    } catch (error) {
+      console.log(error);
     }
+  }
 }
 
-export default new UserController(); 
+export default new UserController();
 
 /*
 index -> list all users -> GET
