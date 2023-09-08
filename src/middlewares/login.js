bbc3a0f62068274bcff3f6006afc89a829ac1884
Enoque Rogerio <enoquerogerio15mti25@gmail.com>
2023-09-08 03:16:42 +0100

    update user payload

--- src/middlewares/login.js
+++ src/middlewares/login.js
@@ -10,13 +10,12 @@ export default (req, res, next) =>{
     const [text, token] = authorization.split(" ");
     
     try {
-        
         const decoded = jwt.verify(token, process.env.TOKEN_KEY);
-        //const {id, email} = decoded
-        console.log(decoded)
+        const { id, email } = decoded
+        req.id = id;
+        req.email = email;
         return next();
     } catch (error) {
-        console.log(error)
         return res.status(401).json({message: "Invalid token"})
     }
 };
